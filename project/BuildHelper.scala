267c4535f3643aacb215464f30b9076ee1abc15e
jczuchnowski <jakub.czuchnowski@gmail.com>
2019-11-30 16:30:59 +0100

    initial design and sbt setup

---
+++ project/BuildHelper.scala
@@ -0,0 +1,81 @@
+import sbt._
+import sbt.Keys._
+
+object BuildHelper {
+
+  def stdSettings(prjName: String) = Seq(
+    name := s"$prjName",
+    scalacOptions := stdOptions,
+    crossScalaVersions := Seq(Scala212, Scala213),
+    scalaVersion in ThisBuild := Scala212,
+    scalacOptions := stdOptions ++ extraOptions(scalaVersion.value),
+    libraryDependencies ++=
+      Seq(
+        ("com.github.ghik" % "silencer-lib" % SilencerVersion % Provided)
+          .cross(CrossVersion.full),
+        compilerPlugin(("com.github.ghik" % "silencer-plugin" % SilencerVersion).cross(CrossVersion.full)),
+        compilerPlugin("org.typelevel" %% "kind-projector" % "0.10.3")
+      ),
+    incOptions ~= (_.withLogRecompileOnMacro(false))
+  )
+
+  private val SilencerVersion = "1.4.4"
+  private val Scala212        = "2.12.10"
+  private val Scala213        = "2.13.1"
+
+  private val stdOptions = Seq(
+    "-encoding",
+    "UTF-8",
+    "-explaintypes",
+    "-Yrangepos",
+    "-feature",
+    "-language:higherKinds",
+    "-language:existentials",
+    "-Xlint:_,-type-parameter-shadow",
+    "-Xsource:2.13",
+    "-Ywarn-dead-code",
+    "-Ywarn-numeric-widen",
+    "-Ywarn-value-discard",
+    "-unchecked",
+    "-deprecation",
+    "-Xfatal-warnings"
+  )
+
+  private val stdOpts213 = Seq(
+    "-Wunused:imports",
+    "-Wvalue-discard",
+    "-Wunused:patvars",
+    "-Wunused:privates",
+    "-Wunused:params",
+    "-Wvalue-discard",
+    "-Wdead-code"
+  )
+
+  private val stdOptsUpto212 = Seq(
+    "-Xfuture",
+    "-Ypartial-unification",
+    "-Ywarn-nullary-override",
+    "-Yno-adapted-args",
+    "-Ywarn-infer-any",
+    "-Ywarn-inaccessible",
+    "-Ywarn-nullary-unit",
+    "-Ywarn-unused-import"
+  )
+
+  private def extraOptions(scalaVersion: String) =
+    CrossVersion.partialVersion(scalaVersion) match {
+      case Some((2, 13)) =>
+        stdOpts213
+      case Some((2, 12)) =>
+        Seq(
+          "-opt-warnings",
+          "-Ywarn-extra-implicit",
+          "-Ywarn-unused:_,imports",
+          "-Ywarn-unused:imports",
+          "-opt:l:inline",
+          "-opt-inline-from:<source>"
+        ) ++ stdOptsUpto212
+      case _ =>
+        Seq("-Xexperimental") ++ stdOptsUpto212
+    }
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,5 @@
+addSbtPlugin("com.dwijnand"              % "sbt-dynver"   % "4.0.0")
+addSbtPlugin("ch.epfl.scala"             % "sbt-bloop"    % "1.3.5")
+addSbtPlugin("org.scalameta"             % "sbt-scalafmt" % "2.2.1")
+addSbtPlugin("io.github.davidgregory084" % "sbt-tpolecat" % "0.1.9")
+addSbtPlugin("org.scalameta"             % "sbt-mdoc"     % "2.0.2")
