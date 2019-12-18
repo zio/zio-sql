20f41cd47764559901eb434e8c6998330c3d6cfa
John A. De Goes <john@degoes.net>
2019-12-18 09:44:37 -0500

    updates

--- src/main/scala/Sql.scala
+++ src/main/scala/Sql.scala
@@ -1,9 +1,50 @@
 package zio.sql
 
-object Sql {
+trait Sql {
   type ColumnName = String
   type TableName
-  type ColumnSchema[_]
+  final case class ColumnSchema[A](value: A)
+
+  sealed trait ColumnSet {
+    type ColumnsRepr[T]
+
+    def :*: [A](head: Column[A]): ColumnSet
+
+    protected def mkColumns[T](table: Table[T]): ColumnsRepr[T]
+  }
+  object ColumnSet {
+    type Empty = Empty.type
+    type :*: [A, B <: ColumnSet] = Cons[A, B]
+
+    case object Empty extends ColumnSet {
+      type ColumnsRepr[_] = Unit
+
+      override def :*: [A](head: Column[A]): A :*: Empty = Cons(head, Empty)
+
+      override protected def mkColumns[T](table: Table[T]): ColumnsRepr[T] = ()
+    }
+    final case class Cons[A, B <: ColumnSet](head: Column[A], tail: B) extends ColumnSet { self =>     
+      type ColumnsRepr[T] = (Expr[T, A], tail.ColumnsRepr[T])
+
+      override def :*: [C](head: Column[C]): C :*: A :*: B = Cons(head, self)
+
+      def columns(table: Table[A :*: B]): ColumnsRepr[A :*: B] = 
+        mkColumns(table)
+
+      override protected def mkColumns[T](table: Table[T]): ColumnsRepr[T] = 
+        (Expr.Source(table, head), tail.mkColumns(table))
+    }
+    object :*: {
+      def unapply[A, B](tuple: (A, B)): Some[(A, B)] = Some(tuple)
+    }
+    import Column._ 
+
+    val columnSet = int("age") :*: string("name") :*: Empty
+
+    lazy val table: Table[Int :*: String :*: Empty] = ???
+
+    val age :*: name :*: _ = columnSet.columns(table)
+  }
 
   sealed trait TypeTag[A]
   object TypeTag {
@@ -13,28 +54,28 @@ object Sql {
     case object TString extends TypeTag[String]
   }
 
-  sealed trait PrimType[A] {
+  sealed trait SqlType[A] {
     def typeTag: TypeTag[A]
   }
-  object PrimType {
-    implicit val SqlTypeBoolean: PrimType[Boolean] = new PrimType[Boolean] {
+  object SqlType {
+    implicit val SqlTypeBoolean: SqlType[Boolean] = new SqlType[Boolean] {
       val typeTag = TypeTag.TBoolean
     }
-    implicit val SqlTypeInt: PrimType[Int] = new PrimType[Int] {
+    implicit val SqlTypeInt: SqlType[Int] = new SqlType[Int] {
       val typeTag = TypeTag.TInt 
     }
-    implicit val SqlTypeLong: PrimType[Long] = new PrimType[Long] {
+    implicit val SqlTypeLong: SqlType[Long] = new SqlType[Long] {
       val typeTag = TypeTag.TLong
     }
-    implicit val SqlTypeString: PrimType[String] = new PrimType[String] {
+    implicit val SqlTypeString: SqlType[String] = new SqlType[String] {
       val typeTag = TypeTag.TString
     }
   }
 
   final case class FunctionName(name: String)
 
-  final case class Column[A: PrimType](name: String) {
-    def typeTag: TypeTag[A] = implicitly[PrimType[A]].typeTag
+  final case class Column[A: SqlType](name: String) {
+    def typeTag: TypeTag[A] = implicitly[SqlType[A]].typeTag
   }
   object Column {
     def int(name: String): Column[Int] = Column[Int](name)
@@ -68,7 +109,7 @@ object Sql {
     
     final case class Union[B](left: Read[B], right: Read[B], distinct: Boolean) extends Read[B]
 
-    final case class Literal[B: PrimType](values: Iterable[B]) extends Read[B]
+    final case class Literal[B: SqlType](values: Iterable[B]) extends Read[B]
   }
 
   /**
@@ -77,7 +118,7 @@ object Sql {
   sealed trait Selection[-A, +B]
   object Selection {
     final case class Identity[A]() extends Selection[A, A]
-    final case class Constant[A: PrimType](value: A) extends Selection[Any, A]
+    final case class Constant[A: SqlType](value: A) extends Selection[Any, A]
     final case class Concat[A, L, R](left: Selection[A, L], right: Selection[A, R]) 
       extends Selection[A, (L, R)]
     final case class Computed[A, B](expr: Expr[A, B], name: Option[ColumnName]) extends 
@@ -109,9 +150,11 @@ object Sql {
   object Expr {
     final case class Source[A, B](table: Table[A], column: Column[B]) extends Expr[A, B]
     final case class Binary[A, B](left: Expr[A, B], right: Expr[A, B], op: BinaryOp[B]) extends Expr[A, B]
-    final case class Literal[B: PrimType](value: B) extends Expr[Any, B]
+    final case class In[A, B](value: Expr[A, B], set: Read[B]) extends Expr[A, Boolean]
+    final case class Literal[B: SqlType](value: B) extends Expr[Any, B]
     final case class FunctionCall[A, B, C](value: Expr[A, B], function: FunctionDef[B, C])
       extends Expr[A, C]
+    // a IN ("foo", "bar")
   }
 
   final case class FunctionDef[-A, +B](name: FunctionName)
