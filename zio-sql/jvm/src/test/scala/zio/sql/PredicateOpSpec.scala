fb6765b375cd4233405d507a36c36935554cc15e
Piotr Paradziński <piotr.paradzinski@scalac.io>
2020-07-28 13:31:10 +0200

    review: rename all queries to query (as in those tests then name is not important)

--- zio-sql/jvm/src/test/scala/zio/sql/PredicateOpSpec.scala
+++ zio-sql/jvm/src/test/scala/zio/sql/PredicateOpSpec.scala
@@ -8,20 +8,20 @@ object PredicateOpSpec extends DefaultRunnableSpec {
 
   def spec = suite("Unary operators")(
     test("isTrue works on boolean column") {
-      val selectDeleted = selectAll.where( deleted.isTrue )
-      assert(selectDeleted)(anything)
+      val query = selectAll.where( deleted.isTrue )
+      assert(query)(anything)
     },
     test("isNotTrue works on boolean column") {
-      val selectDeteltedNotTrue = selectAll.where( deleted.isNotTrue )
-      assert(selectDeteltedNotTrue)(anything)
+      val query = selectAll.where( deleted.isNotTrue )
+      assert(query)(anything)
     },
     test("isNotNull works on column with any type") {
-      val selectProperlyDefined = selectAll.where( name.isNotNull )
-      assert(selectProperlyDefined)(anything)
+      val query = selectAll.where( name.isNotNull )
+      assert(query)(anything)
     },
     test("isNull works on column with any type") {
-      val selectBadlyDefined = selectAll.where( name.isNull )
-      assert(selectBadlyDefined)(anything)
+      val query = selectAll.where( name.isNull )
+      assert(query)(anything)
     }
   )
 }
