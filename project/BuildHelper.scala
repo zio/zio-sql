1916ec27a31bc896b76de5ebd56025848089cb07
jczuchnowski <jakub.czuchnowski@gmail.com>
2020-09-12 19:15:13 +0200

    update Dotty version and fix errors

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -13,7 +13,7 @@ object BuildHelper {
   private val Scala211 = "2.11.12"
   private val Scala212 = "2.12.10"
   private val Scala213 = "2.13.1"
-  val DottyVersion     = "0.23.0-RC1"
+  val DottyVersion     = "0.27.0-RC1"
 
   def buildInfoSettings(packageName: String) =
     Seq(
