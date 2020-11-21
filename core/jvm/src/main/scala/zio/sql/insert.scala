7136d844c5fab8ca4866954890e664786f134dff
jorge.arada <jorge.arada@prozis.com>
2020-11-21 12:59:51 +0000

    fix format

--- core/jvm/src/main/scala/zio/sql/insert.scala
+++ core/jvm/src/main/scala/zio/sql/insert.scala
@@ -4,7 +4,8 @@ trait InsertModule {
   self: ExprModule with TableModule with SelectModule =>
 
   sealed case class InsertBuilder[A](table: Table.Aux[A], filedList: SelectionSet[A]) {
-    def values[Value: TypeTag](valueList: Expr[_, A, Value]): Insert[A] = Insert(table, filedList, valueList :: Nil)
+    def values[Value: TypeTag](valueList: Expr[_, A, Value]): Insert[A] =
+      Insert(table, filedList, valueList :: Nil)
   }
 
   sealed case class Insert[A](table: Table.Aux[A], intoFields: SelectionSet[A], valuesField: List[Expr[_, A, _]]) {
