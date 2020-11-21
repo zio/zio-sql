cd0cfa9f621f17107a1dbd99b7b6d9bb06237452
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2020-11-21 20:56:51 +0100

    Add test for 'Abs' functions for PostgreSQL

--- postgres/src/test/scala/zio/sql/postgresql/FunctionDefSpec.scala
+++ postgres/src/test/scala/zio/sql/postgresql/FunctionDefSpec.scala
@@ -11,6 +11,19 @@ object FunctionDefSpec extends PostgresRunnableSpec with ShopSchema {
   import this.FunctionDef._
 
   val spec = suite("Postgres FunctionDef")(
+    testM("abs") {
+      val query = select(Abs(-3.14159)) from customers
+
+      val expected = 3.14159
+
+      val testResult = execute(query).to[Double, Double](identity)
+
+      val assertion = for {
+        r <- testResult.runCollect
+      } yield assert(r.head)(equalTo(expected))
+
+      assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
+    },
     testM("sin") {
       val query = select(Sin(1.0)) from customers
 
