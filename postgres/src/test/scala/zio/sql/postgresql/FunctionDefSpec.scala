fcd59a8576fff3d01ee0f996937faf964101eb63
angelicarao <angelicarao.ar@gmail.com>
2020-11-21 13:02:08 +0100

    PostgreSQL support for 'reverse'.

--- postgres/src/test/scala/zio/sql/postgresql/FunctionDefSpec.scala
+++ postgres/src/test/scala/zio/sql/postgresql/FunctionDefSpec.scala
@@ -11,6 +11,19 @@ object FunctionDefSpec extends PostgresRunnableSpec with ShopSchema {
   import this.FunctionDef._
 
   val spec = suite("Postgres FunctionDef")(
+    testM("reverse") {
+      val query = select(Reverse("'abcd'")) from customers
+
+      val expected = "dcba"
+
+      val testResult = execute(query).to[String, String](identity)
+
+      val assertion = for {
+        r <- testResult.runCollect
+      } yield assert(r.head)(equalTo(expected))
+
+      assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
+    },
     testM("sin") {
       val query = select(Sin(1.0)) from customers
 
