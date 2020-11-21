6bc3ed70677c70143ce7387295edc91e0a48024c
Marek Kidon <marek.kidon@gmail.com>
2020-11-21 16:14:38 +0100

    Revert "Fix 2.12 tests"
    This reverts commit 9d8e5b6732a0408a7956becadb0ee4fe8700c21e.

--- core/jvm/src/main/scala/zio/sql/delete.scala
+++ core/jvm/src/main/scala/zio/sql/delete.scala
@@ -2,10 +2,10 @@ package zio.sql
 
 trait DeleteModule { self: ExprModule with TableModule =>
 
-  sealed case class DeleteBuilder[A](table: Table.Aux[A]) {
-    def where[F](expr: Expr[F, A, Boolean]): Delete[F, A] = Delete(table, expr)
+  sealed case class DeleteBuilder[F[_], A, B](table: Table.Aux[A]) {
+    def where[F1](expr: Expr[F1, A, Boolean]): Delete[F1, A] = Delete(table, expr)
 
-    def all[F]: Delete[Features.Literal, A] = Delete(table, Expr.literal(true))
+    def all[F1]: Delete[Features.Literal, A] = Delete(table, Expr.literal(true))
   }
 
   sealed case class Delete[F, A](table: Table.Aux[A], whereExpr: Expr[F, A, Boolean])
