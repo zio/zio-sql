81f4c2b87444271a33715c8519a0b37eabdecb62
mehulumistry <mehulumistry20101995@gmail.com>
2020-11-20 22:45:16 -0500

    add postgres test for power, mod

--- postgres/src/test/scala/zio/sql/postgresql/FunctionDefSpec.scala
+++ postgres/src/test/scala/zio/sql/postgresql/FunctionDefSpec.scala
@@ -49,6 +49,32 @@ object FunctionDefSpec extends PostgresRunnableSpec with ShopSchema {
       } yield assert(r.head)(equalTo(expected))
 
       assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
+    },
+    testM("power") {
+      val query = select(Power(7.0, 3.0)) from customers
+
+      val expected = 343.000000000000000
+
+      val testResult = execute(query).to[Double, Double](identity)
+
+      val assertion = for {
+        r <- testResult.runCollect
+      } yield assert(r.head)(equalTo(expected))
+
+      assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
+    },
+    testM("mod") {
+      val query = select(Mod(-15.0, -4.0)) from customers
+
+      val expected = -3.0
+
+      val testResult = execute(query).to[Double, Double](identity)
+
+      val assertion = for {
+        r <- testResult.runCollect
+      } yield assert(r.head)(equalTo(expected))
+
+      assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
     }
   )
 }
