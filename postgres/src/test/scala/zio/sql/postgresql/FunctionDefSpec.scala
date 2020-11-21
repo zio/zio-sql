8be028f9bc9e9d1bf38cd8a94ddda43f95884d48
riccardocorbella <739397@gmail.com>
2020-11-21 19:27:15 +0100

    Add 'localtime' function to PostgresModule

--- postgres/src/test/scala/zio/sql/postgresql/FunctionDefSpec.scala
+++ postgres/src/test/scala/zio/sql/postgresql/FunctionDefSpec.scala
@@ -1,5 +1,7 @@
 package zio.sql.postgresql
 
+import java.time.LocalTime
+
 import zio.Cause
 import zio.test._
 import zio.test.Assertion._
@@ -34,6 +36,28 @@ object FunctionDefSpec extends PostgresRunnableSpec with ShopSchema {
       val assertion = for {
         r <- testResult.runCollect
       } yield assert(r.head)(equalTo(expected))
+
+      assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
+    },
+    testM("localtime") {
+      val query = select(Localtime()) from customers
+
+      val testResult = execute(query).to[LocalTime, LocalTime](identity)
+
+      val assertion = for {
+        r <- testResult.runCollect
+      } yield assert(r.head.toString)(Assertion.matchesRegex("([0-9]{2}):[0-9]{2}:[0-9]{2}\\.[0-9]{3}"))
+
+      assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
+    },
+    testM("localtime with precision") {
+      val query = select(LocaltimeWithPrecision(0)) from customers
+
+      val testResult = execute(query).to[LocalTime, LocalTime](identity)
+
+      val assertion = for {
+        r <- testResult.runCollect
+      } yield assert(r.head.toString)(Assertion.matchesRegex("([0-9]{2}):[0-9]{2}:[0-9]{2}"))
 
       assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
     }
