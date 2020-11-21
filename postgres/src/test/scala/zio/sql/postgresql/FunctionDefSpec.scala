f7306c4a283f500b1d925b1ad550c00667a38fdd
Patryk <pjazdzewski1990@gmail.com>
2020-11-21 09:49:30 +0100

    first draft

--- postgres/src/test/scala/zio/sql/postgresql/FunctionDefSpec.scala
+++ postgres/src/test/scala/zio/sql/postgresql/FunctionDefSpec.scala
@@ -9,8 +9,24 @@ object FunctionDefSpec extends PostgresRunnableSpec with ShopSchema {
   import this.Customers._
   import this.PostgresFunctionDef._
   import this.FunctionDef._
+  import this.ColumnSet._
 
   val spec = suite("Postgres FunctionDef")(
+    testM("concat_ws") {
+      import FlatValueOrColumnValue._
+      val foo: Seq[FlatValueOrColumnValue] = string("first_name") :: string("last_name") :: "!" :: Nil
+      val query = select(ConcatWs(" ", string("first_name") :: string("last_name") :: "!" :: Nil)) from customers
+
+      val expected = Seq("Alice Smith!", "John Smith!")
+
+      val testResult = execute(query).to[String, String](identity)
+
+      val assertion = for {
+        r <- testResult.runCollect
+      } yield assert(r)(equalTo(expected))
+
+      assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
+    },
     testM("sin") {
       val query = select(Sin(1.0)) from customers
 
