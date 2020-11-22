4b690eb04fd7c77bc7f4169faa94b56cece84460
mehulumistry <mehulumistry20101995@gmail.com>
2020-11-21 23:30:35 -0500

    add readRender for oracle and mysql

---
+++ oracle/src/test/scala/zio/sql/oracle/FunctionDefSpec.scala
@@ -0,0 +1,40 @@
+package zio.sql.oracle
+
+import zio.Cause
+import zio.test._
+import zio.test.Assertion._
+
+object FunctionDefSpec extends OracleRunnableSpec with ShopSchema {
+
+  import this.Customers._
+  import this.FunctionDef._
+
+  val spec = suite("Mysql FunctionDef")(
+    testM("sin") {
+      val query = select(Sin(1.0)) from customers
+
+      val expected = 0.8414709848078965
+
+      val testResult = execute(query).to[Double, Double](identity)
+
+      val assertion = for {
+        r <- testResult.runCollect
+      } yield assert(r.head)(equalTo(expected))
+
+      assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
+    },
+    testM("abs") {
+      val query = select(Abs(-32.0)) from customers
+
+      val expected = 32.0
+
+      val testResult = execute(query).to[Double, Double](identity)
+
+      val assertion = for {
+        r <- testResult.runCollect
+      } yield assert(r.head)(equalTo(expected))
+
+      assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
+    }
+  )
+}
