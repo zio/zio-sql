1061f6dc00b3528977f62d6a3491955a9c20ee30
Mahamed Ali <mohamed.tum@gmail.com>
2020-11-22 08:32:28 +0000

    ADDED parse_ident function (line) and a couple of tests

--- postgres/src/test/scala/zio/sql/postgresql/FunctionDefSpec.scala
+++ postgres/src/test/scala/zio/sql/postgresql/FunctionDefSpec.scala
@@ -3,6 +3,7 @@ package zio.sql.postgresql
 import zio.Cause
 import zio.test._
 import zio.test.Assertion._
+import zio.random.Random
 
 object FunctionDefSpec extends PostgresRunnableSpec with ShopSchema {
 
@@ -34,6 +35,37 @@ object FunctionDefSpec extends PostgresRunnableSpec with ShopSchema {
       val assertion = for {
         r <- testResult.runCollect
       } yield assert(r.head)(equalTo(expected))
+
+      assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
+    },
+    testM("parseIdent removes quoting of individual identifiers") {
+      val someString: Gen[Random with Sized, String]    = Gen.anyString
+        .filter(x => x.length < 50 && x.length > 1)
+      //NOTE: I don't know if property based testing is worth doing here, I just wanted to try it
+      val genTestString: Gen[Random with Sized, String] =
+        for {
+          string1 <- someString
+          string2 <- someString
+        } yield s"""'"${string1}".${string2}'"""
+
+      val assertion = checkM(genTestString) { (testString) =>
+        val query      = select(ParseIdent(testString)) from customers
+        val testResult = execute(query).to[String, String](identity)
+
+        for {
+          r <- testResult.runCollect
+        } yield assert(r.head)(not(containsString("'")) && not(containsString("\"")))
+
+      }
+      assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
+    },
+    testM("parseIdent fails with invalid identifier") {
+      val query      = select(ParseIdent("\'\"SomeSchema\".someTable.\'")) from customers
+      val testResult = execute(query).to[String, String](identity)
+
+      val assertion = for {
+        r <- testResult.runCollect.run
+      } yield assert(r)(fails(anything))
 
       assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
     }
