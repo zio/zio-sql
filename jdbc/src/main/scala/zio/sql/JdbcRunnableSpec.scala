fac9188575664fc0ab410da2b61fcc13cf75bc2f
Maciej Bak <maciej.bak.85@gmail.com>
2021-01-08 13:36:51 +0000

    Common code in jdbc runnable

--- jdbc/src/main/scala/zio/sql/JdbcRunnableSpec.scala
+++ jdbc/src/main/scala/zio/sql/JdbcRunnableSpec.scala
@@ -2,6 +2,9 @@ package zio.sql
 
 import zio.test.environment.TestEnvironment
 import zio.test.DefaultRunnableSpec
+import zio.ZLayer
+import zio.blocking.Blocking
+import zio.Has
 
 trait JdbcRunnableSpec extends DefaultRunnableSpec with Jdbc {
 
@@ -11,4 +14,15 @@ trait JdbcRunnableSpec extends DefaultRunnableSpec with Jdbc {
     with DeleteExecutor
     with TransactionExecutor
 
+  val poolConfigLayer: ZLayer[Blocking, Throwable, Has[ConnectionPool.Config]]
+
+  final lazy val executorLayer = {
+    val connectionPoolLayer = ZLayer.identity[Blocking] >+> poolConfigLayer >>> ConnectionPool.live
+
+    (ZLayer.identity[
+      Blocking
+    ] ++ connectionPoolLayer >+> ReadExecutor.live >+> UpdateExecutor.live >+> DeleteExecutor.live >+> TransactionExecutor.live).orDie
+  }
+
+  final lazy val jdbcLayer = TestEnvironment.live >+> executorLayer
 }
--- postgres/src/test/scala/zio/sql/postgresql/PostgresRunnableSpec.scala
+++ postgres/src/test/scala/zio/sql/postgresql/PostgresRunnableSpec.scala
@@ -5,8 +5,6 @@ import zio.test.environment.TestEnvironment
 import java.util.Properties
 import zio.sql.TestContainer
 import zio.Has
-import zio.ZLayer
-import zio.blocking.Blocking
 import zio.sql.JdbcRunnableSpec
 
 trait PostgresRunnableSpec extends JdbcRunnableSpec with PostgresModule {
@@ -18,21 +16,12 @@ trait PostgresRunnableSpec extends JdbcRunnableSpec with PostgresModule {
     props
   }
 
-  lazy val executorLayer = {
-    val poolConfigLayer = TestContainer
-      .postgres()
-      .map(a => Has(ConnectionPool.Config(a.get.jdbcUrl, connProperties(a.get.username, a.get.password))))
+  val poolConfigLayer = TestContainer
+    .postgres()
+    .map(a => Has(ConnectionPool.Config(a.get.jdbcUrl, connProperties(a.get.username, a.get.password))))
 
-    val connectionPoolLayer = ZLayer.identity[Blocking] >+> poolConfigLayer >>> ConnectionPool.live
-
-    (ZLayer.identity[
-      Blocking
-    ] ++ connectionPoolLayer >+> ReadExecutor.live >+> UpdateExecutor.live >+> DeleteExecutor.live >+> TransactionExecutor.live).orDie
-  }
-
-  private val layer = TestEnvironment.live >+> executorLayer
-
-  override def spec: Spec[TestEnvironment, TestFailure[Any], TestSuccess] = specLayered.provideCustomLayerShared(layer)
+  override def spec: Spec[TestEnvironment, TestFailure[Any], TestSuccess] =
+    specLayered.provideCustomLayerShared(jdbcLayer)
 
   def specLayered: Spec[JdbcEnvironment, TestFailure[Object], TestSuccess]
 
