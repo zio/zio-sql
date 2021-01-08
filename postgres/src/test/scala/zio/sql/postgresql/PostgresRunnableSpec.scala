fd457ddce845a54b11ed14d52ef05b4499d1778a
Maciej Bak <maciej.bak.85@gmail.com>
2021-01-08 12:26:03 +0000

    Common layer

--- postgres/src/test/scala/zio/sql/postgresql/PostgresRunnableSpec.scala
+++ postgres/src/test/scala/zio/sql/postgresql/PostgresRunnableSpec.scala
@@ -1,6 +1,18 @@
 package zio.sql.postgresql
 
 import zio.sql.Jdbc
-import zio.test.DefaultRunnableSpec
+import zio.test._
+import zio.test.environment.TestEnvironment
+import zio.Has
 
-trait PostgresRunnableSpec extends DefaultRunnableSpec with Jdbc with PostgresModule with JdbcExecutorLayer
+trait PostgresRunnableSpec extends DefaultRunnableSpec with Jdbc with PostgresModule with JdbcExecutorLayer {
+
+  private val layer = TestEnvironment.live >+> executorLayer
+
+  override def spec: Spec[TestEnvironment, TestFailure[Any], TestSuccess] = specLayered.provideCustomLayerShared(layer)
+
+  def specLayered: Spec[TestEnvironment with Has[TransactionExecutor.Service] with Has[
+    ReadExecutor.Service
+  ], TestFailure[Object], TestSuccess]
+
+}
