61dc2e878df53a11cd2d91a38eceef6d5240e67f
Amr Kamel <3amr.kaml@gmail.com>
2022-05-26 19:08:03 +0200

    Add the 'rpad' function to the MysqlSqlModule (#682)
    * Add the 'rpad' function to the MysqlSqlModule
    
    * fix merge mistake

--- mysql/src/test/scala/zio/sql/mysql/FunctionDefSpec.scala
+++ mysql/src/test/scala/zio/sql/mysql/FunctionDefSpec.scala
@@ -198,6 +198,12 @@ object FunctionDefSpec extends MysqlRunnableSpec with ShopSchema {
 
       assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
     },
+    test("rpad") {
+      val cases = Seq(("hi", 5, "?", "hi???"), ("hi", 1, "?", "h"))
+      check(Gen.fromIterable(cases)) { case (str, len, pad, exp) =>
+        assertZIO(execute(select(RPad(str, len, pad))).runHead.some)(equalTo(exp))
+      }
+    },
     test("current_time") {
       assertZIO(
         execute(select(CurrentTime)).runHead.some
