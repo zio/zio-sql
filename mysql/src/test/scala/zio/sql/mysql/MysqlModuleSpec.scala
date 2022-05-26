4fa42fb46bf28c724860a60891822a7913b6d9e5
Sheng-Loong Su <sushengloong@gmail.com>
2022-05-26 23:35:32 +0800

    Implement renderInsert for MySQL

--- mysql/src/test/scala/zio/sql/mysql/MysqlModuleSpec.scala
+++ mysql/src/test/scala/zio/sql/mysql/MysqlModuleSpec.scala
@@ -1,9 +1,11 @@
 package zio.sql.mysql
 
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.UUID
 
 import zio.Cause
+import zio.schema._
 import zio.test._
 import zio.test.Assertion._
 import scala.language.postfixOps
@@ -160,6 +162,49 @@ object MysqlModuleSpec extends MysqlRunnableSpec with ShopSchema {
       } yield assert(r)(hasSameElementsDistinct(expected))
 
       assertion.mapErrorCause(cause => Cause.stackless(cause.untraced))
+    },
+    test("Can insert rows") {
+      final case class CustomerRow(
+        id: UUID,
+        dateOfBirth: LocalDate,
+        firstName: String,
+        lastName: String,
+        verified: Boolean
+      )
+      implicit val customerRowSchema =
+        Schema.CaseClass5[UUID, LocalDate, String, String, Boolean, CustomerRow](
+          Schema.Field("id", Schema.primitive[UUID](zio.schema.StandardType.UUIDType)),
+          Schema.Field(
+            "dateOfBirth",
+            Schema.primitive[LocalDate](zio.schema.StandardType.LocalDateType(DateTimeFormatter.ISO_DATE))
+          ),
+          Schema.Field("firstName", Schema.primitive[String](zio.schema.StandardType.StringType)),
+          Schema.Field("lastName", Schema.primitive[String](zio.schema.StandardType.StringType)),
+          Schema.Field("verified", Schema.primitive[Boolean](zio.schema.StandardType.BoolType)),
+          CustomerRow.apply,
+          _.id,
+          _.dateOfBirth,
+          _.firstName,
+          _.lastName,
+          _.verified
+        )
+
+      val rows = List(
+        CustomerRow(UUID.randomUUID(), LocalDate.ofYearDay(2001, 8), "Peter", "Parker", true),
+        CustomerRow(UUID.randomUUID(), LocalDate.ofYearDay(1980, 2), "Stephen", "Strange", false)
+      )
+
+      val command = insertInto(customers)(
+        customerId,
+        dob,
+        fName,
+        lName,
+        verified
+      ).values(rows)
+
+      println(renderInsert(command))
+
+      assertZIO(execute(command))(equalTo(2))
     },
     test("Can update rows") {
       val query = update(customers).set(fName, "Roland").where(fName === "Ronald")
