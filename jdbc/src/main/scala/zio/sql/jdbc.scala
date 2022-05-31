1ad4005d7dbba9eea34e92776021a71da7f410fa
balanka <batexy@gmail.com>
2022-05-31 15:06:30 +0200

    Implented issue/feature 645

--- jdbc/src/main/scala/zio/sql/jdbc.scala
+++ jdbc/src/main/scala/zio/sql/jdbc.scala
@@ -8,15 +8,22 @@ trait Jdbc extends zio.sql.Sql with TransactionModule with JdbcInternalModule wi
   trait SqlDriver  {
     def delete(delete: Delete[_]): IO[Exception, Int]
 
+    def delete(delete: List[Delete[_]]): IO[Exception, List[Int]]
+
     def update(update: Update[_]): IO[Exception, Int]
+
+    def update(update: List[Update[_]]): IO[Exception, List[Int]]
 
     def read[A](read: Read[A]): Stream[Exception, A]
 
     def transact[R, A](tx: ZTransaction[R, Exception, A]): ZIO[R, Throwable, A]
 
     def insert[A: Schema](insert: Insert[_, A]): IO[Exception, Int]
+
+    def insert[A: Schema](insert: List[Insert[_, A]]): IO[Exception, List[Int]]
   }
   object SqlDriver {
+
     val live: ZLayer[ConnectionPool, Nothing, SqlDriver] =
       ZLayer(ZIO.serviceWith[ConnectionPool](new SqlDriverLive(_)))
   }
@@ -32,9 +39,18 @@ trait Jdbc extends zio.sql.Sql with TransactionModule with JdbcInternalModule wi
   def execute(delete: Delete[_]): ZIO[SqlDriver, Exception, Int] =
     ZIO.serviceWithZIO(_.delete(delete))
 
+  def executeBatchDelete(delete: List[Delete[_]]): ZIO[SqlDriver, Exception, List[Int]] =
+    ZIO.serviceWithZIO(_.delete(delete))
+
   def execute[A: Schema](insert: Insert[_, A]): ZIO[SqlDriver, Exception, Int] =
     ZIO.serviceWithZIO(_.insert(insert))
 
+  def executeBatchInsert[A: Schema](insert: List[Insert[_, A]]): ZIO[SqlDriver, Exception, List[Int]] =
+    ZIO.serviceWithZIO(_.insert(insert))
+
   def execute(update: Update[_]): ZIO[SqlDriver, Exception, Int] =
+    ZIO.serviceWithZIO(_.update(update))
+
+  def executeBatchUpdate(update: List[Update[_]]): ZIO[SqlDriver, Exception, List[Int]] =
     ZIO.serviceWithZIO(_.update(update))
 }
