7e04aaaf4310ff16b484fca07607b2cdc7410143
balanka <batexy@gmail.com>
2023-07-28 11:00:44 +0200

    Reformated the code

--- postgres/src/test/scala/zio/sql/postgresql/TransactionSpec.scala
+++ postgres/src/test/scala/zio/sql/postgresql/TransactionSpec.scala
@@ -6,8 +6,7 @@ import zio.test.Assertion._
 import zio.test._
 import zio.test.TestAspect.sequential
 
-//import java.sql.BatchUpdateException
-import java.time.{LocalDate, ZonedDateTime}
+import java.time.{ LocalDate, ZonedDateTime }
 import java.util.UUID
 
 object TransactionSpec extends PostgresRunnableSpec with DbSchema {
@@ -15,11 +14,6 @@ object TransactionSpec extends PostgresRunnableSpec with DbSchema {
   override val autoCommit = false
 
   import CustomerSchema._
-
-  private def update_(c: Customer): Update[customers.TableType] =
-    update(customers)
-      .set(verified, !c.verified)
-      .where(customerId === c.id)
 
   override def specLayered = suite("Postgres module")(
     test("Transaction is returning the last value") {
@@ -49,10 +43,10 @@ object TransactionSpec extends PostgresRunnableSpec with DbSchema {
     },
     test("Transaction succeeded and deleted rows") {
       val deleteQuery = deleteFrom(customers).where(verified === false)
-      val id1 = UUID.randomUUID()
-      val id2 = UUID.randomUUID()
+      val id1         = UUID.randomUUID()
+      val id2         = UUID.randomUUID()
 
-      val c1 = Customer(
+      val c1          = Customer(
         id1,
         LocalDate.now(),
         "fnameCustomer1",
@@ -61,7 +55,7 @@ object TransactionSpec extends PostgresRunnableSpec with DbSchema {
         LocalDate.now().toString,
         ZonedDateTime.now()
       )
-      val c2 = Customer(
+      val c2          = Customer(
         id2,
         LocalDate.now(),
         "fnameCustomer2",
@@ -71,27 +65,27 @@ object TransactionSpec extends PostgresRunnableSpec with DbSchema {
         ZonedDateTime.now()
       )
       val allCustomer = List(c1, c2)
-      val data = allCustomer.map(Customer.unapply(_).get)
-      val insertStmt = insertInto(customers)(ALL).values(data)
-      val updateStmt = allCustomer.map(update_)
+      val data        = allCustomer.map(Customer.unapply(_).get)
+      val insertStmt  = insertInto(customers)(ALL).values(data)
+      val updateStmt  = allCustomer.map(update_)
 
       val batchResult = for {
-        deleted<-deleteQuery.run
-        inserted<-insertStmt.run
-        updated<-updateStmt.run
-      }yield deleted+inserted+updated
+        deleted  <- deleteQuery.run
+        inserted <- insertStmt.run
+        updated  <- updateStmt.run
+      } yield deleted + inserted + updated
 
       val result = for {
-        tx  <- transact(batchResult)
-      } yield  tx
+        tx <- transact(batchResult)
+      } yield tx
       assertZIO(result)(equalTo(5)).mapErrorCause(cause => Cause.stackless(cause.untraced))
     },
-      test ("Transaction failed and no row was inserted updated or deleted") {
+    test("Transaction failed and no row was inserted updated or deleted") {
       val deleteQuery = deleteFrom(customers).where(verified === false)
-      val id1 = UUID.randomUUID()
-      //val id2 = UUID.randomUUID()
+      val id1         = UUID.randomUUID()
+      // val id2 = UUID.randomUUID()
 
-      val c1 = Customer(
+      val c1          = Customer(
         id1,
         LocalDate.now(),
         "fnameCustomer1",
@@ -100,7 +94,7 @@ object TransactionSpec extends PostgresRunnableSpec with DbSchema {
         LocalDate.now().toString,
         ZonedDateTime.now()
       )
-      val c2 = Customer(
+      val c2          = Customer(
         id1,
         LocalDate.now(),
         "fnameCustomer2",
@@ -110,23 +104,28 @@ object TransactionSpec extends PostgresRunnableSpec with DbSchema {
         ZonedDateTime.now()
       )
       val allCustomer = List(c1, c2)
-      val data = allCustomer.map(Customer.unapply(_).get)
-      val insertStmt = insertInto(customers)(ALL).values(data)
-      val updateStmt = allCustomer.map(update_)
+      val data        = allCustomer.map(Customer.unapply(_).get)
+      val insertStmt  = insertInto(customers)(ALL).values(data)
+      val updateStmt  = allCustomer.map(update_)
 
       val batchResult = for {
         deleted <- deleteQuery.run
-        //inserted <- insertStmt.run
-        _ <- ZIO.fail(insertStmt.run).exit
+        // inserted <- insertStmt.run
+        _       <- ZIO.fail(insertStmt.run).exit
         updated <- updateStmt.run
 
-      } yield deleted+updated //+ inserted
+      } yield deleted + updated // + inserted
 
       val result = (for {
         tx <- transact(batchResult)
       } yield tx).flip.exit
-        assertZIO(result) (fails((anything)))
+      assertZIO(result)(fails((anything)))
 
     }
   ) @@ sequential
+
+  private def update_(c: Customer): Update[customers.TableType] =
+    update(customers)
+      .set(verified, !c.verified)
+      .where(customerId === c.id)
 }
