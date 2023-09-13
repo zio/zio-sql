9bd158d3b421fdeb6c84e825739d75a942dfe83d
Scala Steward <scala_steward@virtuslab.com>
2023-09-13 10:14:16 +0000

    Update scala-library, scala-reflect to 2.13.12

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -11,7 +11,7 @@ import scalafix.sbt.ScalafixPlugin.autoImport.scalafixSemanticdb
 object BuildHelper {
   val SilencerVersion = "1.17.13"
   val Scala212        = "2.12.18"
-  val Scala213        = "2.13.10"
+  val Scala213        = "2.13.12"
   val ScalaDotty      = "3.3.0"
 
   def buildInfoSettings(packageName: String) =
